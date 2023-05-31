7f275d0e0f0ee6dc8b5a60225c2b2cfe7677c516
Huu-Thuc Cai <huuthuc997@gmail.com>
2023-05-31 21:42:56 +0700

    update blinky main.c

--- Blinky/main.c
+++ Blinky/main.c
@@ -1,9 +1,6 @@
 #include "stm32l152xe.h"
-#include "stm32l1xx.h"
 
 #define LED_NUM                      (1)
-
-const unsigned long led_mask[] = {1ul << 5};
 
 volatile uint32_t msTicks;                                 // counts 1ms timeTicks
 
@@ -13,36 +10,76 @@ void SysTick_Handler(void) {
   msTicks++;
 }
 
-int main() {
+int main (void) {
 
-  /*Configure the builtin LED LD2 at GPIO PA5 pin*/
+  SystemCoreClockUpdate();
+  SysTick_Config(SystemCoreClock / 1000); 
+
+  /* Configure LED (PA.5) pins as GPIO push-pull outputs, No pull-up, pull-down */
   RCC->AHBENR |=  RCC_AHBENR_GPIOAEN; /* Enable GPIOA clock         */
 
-  /* Configure LED (PA.5) pins as push-pull outputs, No pull-up, pull-down */
-  GPIOA->MODER   &= ~((3ul << 2*5));
-  GPIOA->MODER   |=  ((1ul << 2*5));
-  GPIOA->OTYPER  &= ~((1ul <<   5));
-  GPIOA->OSPEEDR &= ~((3ul << 2*5));
-  GPIOA->OSPEEDR |=  ((1ul << 2*5));
-  GPIOA->PUPDR   &= ~((3ul << 2*5));
+  /*  GPIO_MODER    : GPIO port mode register 
+      These bits are written by software to configure the I/O direction mode.
+          00: Input (reset state)
+          01: General purpose output mode
+          10: Alternate function mode
+          11: Analog mode   
+  */
+  GPIOA->MODER  &= ~GPIO_MODER_MODER5;      /*  Reset the value of GPIOA_MODER at PA5 */
+  GPIOA->MODER  |= GPIO_MODER_MODER5_0;     /*  Turn PA5 into 01 -- GPIO mode */
+
+  /*  GPIO_OTYPR    : GPIO port output type register
+      Bits 31:16 Reserved, must be kept at reset value.
+      Bits 15:0 OTy: Port x configuration bits (y = 0..15)
+        These bits are written by software to configure the output type of the I/O port.
+          0: Output push-pull (reset state)
+          1: Output open-drain
+  */
+  GPIOA->OTYPER &= ~GPIO_OTYPER_OT_5;     /* Reset the value of GPIOA_OTYPR at PA5*/
 
 
-  while (1);
+  /*  GPIO_OSPEEDR  : GPIO port output speed register
+      These bits are written by software to configure the I/O output speed.
+        00: Low speed
+        01: Medium speed
+        10: High speed
+        11: Very high speed
+  */
+  GPIOA->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR5; /* Reset the value of GPIOA_OSPEEDR at PA5 */
 
-  // for (;;) {
-  //   LED_On(num);                                           // Turn specified LED on
-  //   Delay(500);                                            // Wait 500ms
-  //   while (Buttons_GetState() & (1 << 0));                 // Wait while holding USER button
-  //   LED_Off(num);                                          // Turn specified LED off
-  //   Delay(500);                                            // Wait 500ms
-  //   while (Buttons_GetState() & (1 << 0));                 // Wait while holding USER button
+  /*  GPIO_PUPDR     : GPIO port pull-up/pull-down register\
+      These bits are written by software to configure the I/O pull-up or pull-down
+        00: No pull-up, pull-down
+        01: Pull-up
+        10: Pull-down
+        11: Reserved
+  */
+  GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR5;       /* Reset the value of GPIOA_PUPDR at PA5 */
 
-  //   num++;                                                 // Change LED number
-  //   if (num >= max_num) {
-  //     num = 0;                                             // Restart with first LED
-  //   }
-  // }
+  
+	/* Configure Builtin Black Button at PC13 as GPIO push-pull outputs, No pull-up, pull-down */
+  RCC->AHBENR |=  RCC_AHBENR_GPIOCEN;                   /* Enable GPIOC clock         */
 
+  GPIOC->MODER   &= ~GPIO_MODER_MODER13;
+  
+	GPIOC->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR13;
+  
+	GPIOC->PUPDR   &= ~GPIO_PUPDR_PUPDR13;
+
+	GPIOA->BSRR = GPIO_BSRR_BS_5;
+  while (1) {
+    /* Turn on the PA.5 LED*/
+    GPIOA->BSRR = GPIO_BSRR_BS_5;
+
+    Delay(500);
+    while (((GPIOC->IDR & GPIO_IDR_IDR_13) == 0) & (1 << 0)); // Wait while holding USER button
+    /* Turn off the PA.5 LED*/
+    GPIOA->BSRR &= ~GPIO_BSRR_BS_5;
+    GPIOA->BSRR = GPIO_BSRR_BR_5;
+    Delay(500);
+    while (((GPIOC->IDR & GPIO_IDR_IDR_13) == 0) & (1 << 0)); // Wait while holding USER button
+
+  }
 }
 
 /*----------------------------------------------------------------------------
@@ -54,197 +91,4 @@ void Delay (uint32_t dlyTicks) {
   curTicks = msTicks;
   while ((msTicks - curTicks) < dlyTicks) { __NOP(); }
 }
-
-// int32_t LED_On (uint32_t num) {
-//   int32_t retCode = 0;
-//   if (num < LED_NUM) {
-//     GPIOA->BSRR = MASK_PORT5;
-//   } else {
-//     retCode = -1;
-//   }
-//   return retCode;
-// }
-
-// int32_t LED_Off (uint32_t num) {
-//   int32_t retCode = 0;
-//   if (num < LED_NUM) {
-//     GPIOA->BSRR &= ~MASK_PORT5;
-//   } else {
-//     retCode = -1;
-//   }
-//   return retCode;
-// }
-
-
-// /**
-//   \fn          int32_t LED_Initialize (void)
-//   \brief       Initialize LEDs
-//   \returns
-//    - \b  0: function succeeded
-//    - \b -1: function failed
-// */
-// int32_t LED_Initialize (void) {
-
-//   RCC->AHBENR |=  (1ul << 0);                   /* Enable GPIOA clock         */
-
-//   /* Configure LED (PA.5) pins as push-pull outputs, No pull-up, pull-down */
-//   GPIOA->MODER   &= ~((3ul << 2*5));
-//   GPIOA->MODER   |=  ((1ul << 2*5));
-//   GPIOA->OTYPER  &= ~((1ul <<   5));
-//   GPIOA->OSPEEDR &= ~((3ul << 2*5));
-//   GPIOA->OSPEEDR |=  ((1ul << 2*5));
-//   GPIOA->PUPDR   &= ~((3ul << 2*5));
-//   return (0);
-// }
-
-
-// /**
-//   \fn          int32_t LED_Uninitialize (void)
-//   \brief       De-initialize I/O interface for LEDs
-//   \returns
-//    - \b  0: function succeeded
-//    - \b -1: function failed
-// */
-// int32_t LED_Uninitialize (void) {
-
-//   return (0);
-// }
-
-
-// /**
-//   \fn          int32_t LED_On (uint32_t num)
-//   \brief       Turn on requested LED
-//   \param[in]   num  LED number
-//   \returns
-//    - \b  0: function succeeded
-//    - \b -1: function failed
-// */
-// int32_t LED_On (uint32_t num) {
-//   int32_t retCode = 0;
-//   if (num < LED_NUM) {
-//     GPIOA->BSRR = (led_mask[num]);
-//   } else {
-//     retCode = -1;
-//   }
-//   return retCode;
-// }
-
-
-// /**
-//   \fn          int32_t LED_Off (uint32_t num)
-//   \brief       Turn off requested LED
-//   \param[in]   num  LED number
-//   \returns
-//    - \b  0: function succeeded
-//    - \b -1: function failed
-// */
-// int32_t LED_Off (uint32_t num) {
-//   int32_t retCode = 0;
-//   if (num < LED_NUM) {
-//     GPIOA->BSRR = (led_mask[num] << 16);
-//   } else {
-//     retCode = -1;
-//   }
-//   return retCode;
-// }
-
-
-// /**
-//   \fn          int32_t LED_SetOut (uint32_t val)
-//   \brief       Write value to LEDs
-//   \param[in]   val  value to be displayed on LEDs
-//   \returns
-//    - \b  0: function succeeded
-//    - \b -1: function failed
-// */
-// int32_t LED_SetOut (uint32_t val) {
-//   int i;
-
-//   for (i = 0; i < LED_NUM; i++) {
-//     if (val & (1<<i)) {
-//       LED_On (i);
-//     } else {
-//       LED_Off(i);
-//     }
-//   }
-//   return (0);
-// }
-
-
-// /**
-//   \fn          uint32_t LED_GetCount (void)
-//   \brief       Get number of LEDs
-//   \return      Number of available LEDs
-// */
-// uint32_t LED_GetCount (void) {
-//   return LED_NUM;
-// }
-
-
-// #define NUM_KEYS  1                     /* Number of available keys           */
-
-// /* Keys for NUCLEO Board */
-// #define USER    1
-
-
-// /**
-//   \fn          int32_t Buttons_Initialize (void)
-//   \brief       Initialize buttons
-//   \returns
-//    - \b  0: function succeeded
-//    - \b -1: function failed
-// */
-// int32_t Buttons_Initialize (void) {
-
-//   RCC->AHBENR |=  (1ul << 2);                   /* Enable GPIOC clock         */
-
-//   GPIOC->MODER   &= ~(3ul << 2*13);
-//   GPIOC->OSPEEDR &= ~(3ul << 2*13);
-//   GPIOC->OSPEEDR |=  (1ul << 2*13);
-//   GPIOC->PUPDR   &= ~(3ul << 2*13);
-//   return (0);
-// }
-
-
-// /**
-//   \fn          int32_t Buttons_Uninitialize (void)
-//   \brief       De-initialize buttons
-//   \returns
-//    - \b  0: function succeeded
-//    - \b -1: function failed
-// */
-// int32_t Buttons_Uninitialize (void) {
-
-//   GPIOC->MODER &= ~(3ul << 2*13);
-//   return (0);
-// }
-
-
-// /**
-//   \fn          uint32_t Buttons_GetState (void)
-//   \brief       Get buttons state
-//   \returns     Buttons state
-// */
-// uint32_t Buttons_GetState (void) {
-
-//   uint32_t val = 0;
-
-//   if ((GPIOC->IDR & (1ul << 13)) == 0) {
-//     /* USER button */
-//     val |= USER;
-//   }
-
-//   return (val);
-// }
-
-
-// /**
-//   \fn          uint32_t Buttons_GetCount (void)
-//   \brief       Get number of available buttons
-//   \return      Number of available buttons
-// */
-// uint32_t Buttons_GetCount (void) {
-//   return (NUM_KEYS);
-// }
-
 
